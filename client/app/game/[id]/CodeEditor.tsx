10dddc664405e37c1e3481ae97a61e926d8f36fe
Kevin Yu <kevinyu567@gmail.com>
2025-02-21 22:58:01 -0500

    Working code editor

--- client/app/game/[id]/CodeEditor.tsx
+++ client/app/game/[id]/CodeEditor.tsx
@@ -1,7 +1,37 @@
+'use client'
+
+import { useState } from 'react';
+import SyntaxHighlighter from '@/components/SyntaxHighlighter';
+
+
 export default function CodeEditor() {
+    const [code, setCode] = useState(ex);
+
     return (
-        <div className="flex-grow bg-editor">
-            // ...
+        <div className="flex-grow bg-editor relative">
+            {/* Hack: invisible textarea to capture user input */}
+            <textarea
+                className="absolute p-[1em] inset-0 text-[13px] font-[Menlo,_Monaco,_Consolas,_'Andale_Mono',_'Ubuntu_Mono',_'Courier_New',_monospace] text-white my-[0.5em] ml-[29.25px] outline-none text-transparent caret-white"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+                spellCheck={false}
+            />
+
+            <SyntaxHighlighter language="javascript">
+                {code}
+            </SyntaxHighlighter>
         </div>
     )
 }
+
+const ex = `import SyntaxHighlighter from '@/components/SyntaxHighlighter';
+
+export default function CodeEditor() {
+    return (
+        <div className="flex-grow bg-editor">
+            <SyntaxHighlighter language="js">
+                function hii
+            </SyntaxHighlighter>
+        </div>
+    )
+}`
---
+++ client/components/SyntaxHighlighter.tsx
@@ -0,0 +1,21 @@
+import { Prism } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+
+type SyntaxHighlighterProps = {
+    language: string,
+    children: string | string[]
+};
+export default function SyntaxHighlighter(props: SyntaxHighlighterProps) {
+    return (
+        <Prism
+            language={props.language}
+            style={vscDarkPlus}
+            // codeTagProps={{style: {}}}
+            showLineNumbers
+            // useInlineStyles={false}
+        >
+            {props.children}
+        </Prism>
+    )
+}
