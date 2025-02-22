818ecdf1727c197c61c4bfc4129209e69b8a76a5
Kevin Yu <kevinyu567@gmail.com>
2025-02-22 02:29:53 -0500

    GNU-ify docs more, add editor layout section

--- client/app/game/[id]/ManualPlayerInterface.tsx
+++ client/app/game/[id]/ManualPlayerInterface.tsx
@@ -4,104 +4,123 @@ import { RuleAndTrigger } from '@/utils/rules';
 
 export default function ManualPlayerInterface() {
     return (
-        <div className="container font-serif py-16">
-            <h1 className="font-bold text-4xl mb-6">
-                The Scame Editor
-            </h1>
-            <p className="mb-4">
-                Scame is the advanced, extensible, customizable, self-documenting editor. This manual describes how to
-                edit with Scame and some of the ways to customize it; it corresponds to KNU Scame version 2025.2.21.
-            </p>
-            <p className="mb-4">
-                This is the KNU Scame Manual, updated for Scame version 2025.2.21.
-            </p>
-            <p className="mb-12">
-                Copyright © 1985–2024 KTANE, Inc.
-            </p>
+        <div className="bg-gray-200">
+            <div className="container font-serif py-16 max-w-6xl bg-white border-x border-black/20">
+                <h1 className="font-bold text-4xl mb-6">
+                    The Scame Editor
+                </h1>
+                <p className="mb-4">
+                    Scame is the advanced, extensible, customizable, self-documenting editor. This manual describes how
+                    to edit with Scame and some of the ways to customize it; it corresponds to KNU Scame version
+                    2025.2.21.
+                </p>
+                <p className="mb-4">
+                    This is the KNU Scame Manual, updated for Scame version 2025.2.21.
+                </p>
+                <p className="mb-12">
+                    Copyright © 1985–2024 KTANE, Inc.
+                </p>
 
-            <h2 className="font-bold text-xl mb-5">
-                Table of contents
-            </h2>
-            <ul className="list-disc pl-6 mb-12">
-                <li className="mb-2">
-                    <a className="text-[#005090] underline" href="#intro">Introduction</a>
-                </li>
-                <li className="mb-2">
-                    <a className="text-[#005090] underline" href="#chars">Characters</a>
-                </li>
-                <li className="mb-2">
-                    <a className="text-[#005090] underline" href="#nav">Navigation</a>
-                </li>
-                <li className="mb-2">
-                    <a className="text-[#005090] underline" href="#bg">Background color</a>
-                </li>
-                <li className="mb-2">
-                    <a className="text-[#005090] underline" href="#mode">Mode</a>
-                </li>
-            </ul>
+                <h2 className="font-bold text-xl mb-5">
+                    Table of contents
+                </h2>
+                <ul className="list-disc pl-6 mb-12">
+                    <li className="mb-2">
+                        <a className="text-[#005090] underline" href="#intro">Introduction</a>
+                    </li>
+                    <li className="mb-2">
+                        <a className="text-[#005090] underline" href="#layout">Editor layout</a>
+                    </li>
+                    <li className="mb-2">
+                        <a className="text-[#005090] underline" href="#chars">Characters</a>
+                    </li>
+                    <li className="mb-2">
+                        <a className="text-[#005090] underline" href="#nav">Navigation</a>
+                    </li>
+                    <li className="mb-2">
+                        <a className="text-[#005090] underline" href="#bg">Background color</a>
+                    </li>
+                    <li className="mb-2">
+                        <a className="text-[#005090] underline" href="#mode">Mode</a>
+                    </li>
+                </ul>
 
-            <h2 className="font-bold text-xl mb-5" id="intro">
-                1. Introduction
-            </h2>
-            <p className="mb-4">
-                You are reading about KNU Scame, the KNU incarnation of the advanced, self-documenting, customizable,
-                extensible editor Scame. (Scame Cannot Accurately Mimic Emacs)
-            </p>
-            <p className="mb-12">
-                We call Scame advanced because it can do much more than simple insertion and deletion of text. It can
-                change background color, switch tabs, and deploy to prod in a single command. Scame
-                editing commands operate in terms of characters, words, lines, sentences, paragraphs, and pages, as well
-                as expressions and comments in various programming languages.
-            </p>
+                <h2 className="font-bold text-xl mb-5" id="intro">
+                    0. Introduction
+                </h2>
+                <p className="mb-4">
+                    You are reading about KNU Scame, the KNU incarnation of the advanced, self-documenting,
+                    customizable, extensible editor Scame. (Scame Cannot Accurately Mimic Emacs)
+                </p>
+                <p className="mb-12">
+                    We call Scame advanced because it can do much more than simple insertion and deletion of text. It
+                    can change background color, switch tabs, and deploy to prod in a single command. Scame
+                    editing commands operate in terms of characters, words, lines, sentences, paragraphs, and pages, as
+                    well as expressions and comments in various programming languages.
+                </p>
 
-            <h2 className="font-bold text-xl mb-5" id="chars">
-                2. Characters
-            </h2>
-            <p className="mb-8">
-                The following keybinds relate to inputting characters:
-            </p>
+                <h2 className="font-bold text-xl mb-5" id="layout">
+                    1. Editor layout
+                </h2>
+                <p className="mb-4">
+                    An example of a Scame editor is as follows:
+                </p>
+                <img src="/assets/docs.jpg" />
+                <p className="mb-12 mt-5">
+                    Within the bottom bar, (1) displays your current <strong>Serial number</strong>, (2) displays your
+                    current <strong>Mode</strong> and <strong>Background color</strong>, and (3) displays your
+                    current <strong>Cursor position</strong>.
+                </p>
 
-            <KeybindDocumentation {...rule} />
-            <KeybindDocumentation {...rule} />
+                <h2 className="font-bold text-xl mb-5" id="chars">
+                    2. Characters
+                </h2>
+                <p className="mb-8">
+                    The following keybinds relate to inputting characters:
+                </p>
 
-            <h2 className="font-bold text-xl mt-12 mb-5" id="nav">
-                3. Navigation
-            </h2>
-            <p className="mb-8">
-                The following keybinds relate to navigating around the currently edited document:
-            </p>
+                <KeybindDocumentation {...rule} />
+                <KeybindDocumentation {...rule} />
 
-            <KeybindDocumentation {...rule} />
-            <KeybindDocumentation {...rule} />
-            <KeybindDocumentation {...rule} />
+                <h2 className="font-bold text-xl mt-12 mb-5" id="nav">
+                    3. Navigation
+                </h2>
+                <p className="mb-8">
+                    The following keybinds relate to navigating around the currently edited document:
+                </p>
 
-            <h2 className="font-bold text-xl mt-12 mb-5" id="bg">
-                4. Background color
-            </h2>
-            <p className="mb-5">
-                Some Scame keybinds automatically enable based on your background color. By default, your background
-                color is <code className="bg-gray-200 rounded px-1.5 py-0.5 text-sm">{res.initial_color}</code>.
-            </p>
-            <p className="mb-8">
-                The following keybinds relate to changing the current background color:
-            </p>
+                <KeybindDocumentation {...rule} />
+                <KeybindDocumentation {...rule} />
+                <KeybindDocumentation {...rule} />
 
-            <KeybindDocumentation {...rule} />
-            <KeybindDocumentation {...rule} />
+                <h2 className="font-bold text-xl mt-12 mb-5" id="bg">
+                    4. Background color
+                </h2>
+                <p className="mb-5">
+                    Some Scame keybinds automatically enable based on your background color. By default, your background
+                    color is <code className="bg-gray-200 rounded px-1.5 py-0.5 text-sm">{res.initial_color}</code>.
+                </p>
+                <p className="mb-8">
+                    The following keybinds relate to changing the current background color:
+                </p>
 
-            <h2 className="font-bold text-xl mt-12 mb-5" id="mode">
-                5. Mode
-            </h2>
-            <p className="mb-5">
-                Scame contains {res.modes.length} unique editing modes: <code className="bg-gray-200 rounded px-1.5 py-0.5 text-sm">{res.modes.join(', ')}</code>.
-                By default, your mode is set to <code className="bg-gray-200 rounded px-1.5 py-0.5 text-sm">{res.initial_mode}</code>.
-            </p>
-            <p className="mb-8">
-                The following keybinds relate to changing the current mode:
-            </p>
+                <KeybindDocumentation {...rule} />
+                <KeybindDocumentation {...rule} />
 
-            <KeybindDocumentation {...rule} />
-            <KeybindDocumentation {...rule} />
+                <h2 className="font-bold text-xl mt-12 mb-5" id="mode">
+                    5. Mode
+                </h2>
+                <p className="mb-5">
+                    Scame contains {res.modes.length} unique editing modes: <code className="bg-gray-200 rounded px-1.5 py-0.5 text-sm">{res.modes.join(', ')}</code>.
+                    By default, your mode is set to <code className="bg-gray-200 rounded px-1.5 py-0.5 text-sm">{res.initial_mode}</code>.
+                </p>
+                <p className="mb-8">
+                    The following keybinds relate to changing the current mode:
+                </p>
+
+                <KeybindDocumentation {...rule} />
+                <KeybindDocumentation {...rule} />
+            </div>
         </div>
     )
 }
