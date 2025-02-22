1c0d8fdb333865474b14a744277e9f68ee58fcfc
Kevin Yu <kevinyu567@gmail.com>
2025-02-21 21:39:53 -0500

    Init with nextjs boilerplate

---
+++ client/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import type { ReactNode } from 'react';
+import { Inter } from 'next/font/google';
+
+import './globals.css';
+
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    title: 'Keep Typing And No One Explodes',
+    description: 'Pair programming simulator in 2025.',
+}
+
+export default function RootLayout(props: { children: ReactNode }) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                {props.children}
+            </body>
+        </html>
+    )
+}
