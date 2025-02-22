074c9f14a055b4af98b1792757693dd72da9a16b
Kevin Yu <kevinyu567@gmail.com>
2025-02-22 01:57:24 -0500

    Python highlighting, add bottom info bar

--- client/app/game/[id]/CodeEditor.tsx
+++ client/app/game/[id]/CodeEditor.tsx
@@ -10,7 +10,7 @@ import FileBar from '@/app/game/[id]/FileBar';
 export default function CodeEditor() {
     const [code, setCode] = useState(ex);
 
-    const files = ['a.txt', 'b.js', 'c.hs'];
+    const files = ['test.py', 'two.py', 'runner.py'];
     const [selected, setSelected] = useState(files[0]);
 
     return (
@@ -21,7 +21,7 @@ export default function CodeEditor() {
                 setSelected={setSelected}
             />
 
-            <div className="bg-editor relative h-full">
+            <div className="bg-editor relative flex-grow overflow-y-auto">
                 {/* Hack: invisible textarea to capture user input */}
                 <textarea
                     className="absolute p-[1em] inset-0 text-[13px] font-[Menlo,_Monaco,_Consolas,_'Andale_Mono',_'Ubuntu_Mono',_'Courier_New',_monospace] ml-[29.25px] outline-none text-transparent caret-white resize-none"
@@ -30,22 +30,35 @@ export default function CodeEditor() {
                     spellCheck={false}
                 />
 
-                <SyntaxHighlighter language="javascript">
+                <SyntaxHighlighter language="python">
                     {code}
                 </SyntaxHighlighter>
+            </div>
+
+            <div className="flex gap-8 bg-filebar text-white/40 py-4 px-5 font-mono text-sm border-t border-white/10">
+                <div>
+                    <p>S: 27ha20vla</p>
+                    <p>KNU Scame</p>
+                </div>
+                <div className="text-right ml-auto">
+                    <p>M cycloid | B purple</p>
+                    <p>C 0,1</p>
+                </div>
+                <div>
+                    All
+                </div>
             </div>
         </div>
     )
 }
 
-const ex = `import SyntaxHighlighter from '@/components/SyntaxHighlighter';
-
-export default function CodeEditor() {
-    return (
-        <div className="flex-grow bg-editor">
-            <SyntaxHighlighter language="js">
-                function hii
-            </SyntaxHighlighter>
-        </div>
-    )
-}`
+const ex = `def serialize_document(doc):
+    """Recursively convert MongoDB documents to JSON-serializable format."""
+    if isinstance(doc, dict):
+        return {k: serialize_document(v) for k, v in doc.items()}
+    elif isinstance(doc, list):
+        return [serialize_document(v) for v in doc]
+    elif isinstance(doc, ObjectId):
+        return str(doc)
+    return doc
+`
