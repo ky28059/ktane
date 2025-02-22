1bba001986ec6df670d5568a80eb19ff1c5d2a53
Kevin Yu <kevinyu567@gmail.com>
2025-02-22 00:12:38 -0500

    Mockup console, doc page

--- client/app/game/[id]/Console.tsx
+++ client/app/game/[id]/Console.tsx
@@ -1,7 +1,20 @@
 export default function Console() {
     return (
-        <div className="bg-black text-white w-[40vw] border-l border-white/25">
-            {'>'} ...
+        <div className="bg-black text-white w-[40vw] border-l border-white/25 p-4">
+            <pre className="whitespace-pre-wrap text-sm">
+                {ex}
+            </pre>
         </div>
     )
 }
+
+const ex = `Welcome to Ubuntu 22.04.4 LTS (GNU/Linux 5.15.153.1-microsoft-standard-WSL2 x86_64)
+
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/pro
+
+
+This message is shown once a day. To disable it please create the
+/home/dev/.hushlogin file.
+dev@ktane:~$`
--- client/app/game/[id]/page.tsx
+++ client/app/game/[id]/page.tsx
@@ -1,7 +1,14 @@
 import CodePlayerInterface from '@/app/game/[id]/CodePlayerInterface';
+import ManualPlayerInterface from '@/app/game/[id]/ManualPlayerInterface';
 
 
-export default function GamePage() {
+export default async function GamePage({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+
+    if (id === '333') return (
+        <ManualPlayerInterface />
+    )
+
     return (
         <CodePlayerInterface />
     )
