05f7b140bdadaa5766ad6cda311df58ba0a7dd57
Kevin Yu <kevinyu567@gmail.com>
2025-02-22 02:03:47 -0500

    Add keyboard event handler

--- client/app/game/[id]/CodeEditor.tsx
+++ client/app/game/[id]/CodeEditor.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // Components
 import SyntaxHighlighter from '@/components/SyntaxHighlighter';
@@ -13,6 +13,18 @@ export default function CodeEditor() {
     const files = ['test.py', 'two.py', 'runner.py'];
     const [selected, setSelected] = useState(files[0]);
 
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            e.preventDefault();
+
+            // JACK: edit below this line!!!
+            console.log(e.code, e.altKey, e.ctrlKey, e.shiftKey, e.metaKey);
+        }
+
+        window.addEventListener('keypress', handler);
+        return () => window.removeEventListener('keypress', handler);
+    }, []);
+
     return (
         <div className="flex-grow flex flex-col">
             <FileBar
@@ -23,12 +35,14 @@ export default function CodeEditor() {
 
             <div className="bg-editor relative flex-grow overflow-y-auto">
                 {/* Hack: invisible textarea to capture user input */}
+                {/*
                 <textarea
                     className="absolute p-[1em] inset-0 text-[13px] font-[Menlo,_Monaco,_Consolas,_'Andale_Mono',_'Ubuntu_Mono',_'Courier_New',_monospace] ml-[29.25px] outline-none text-transparent caret-white resize-none"
                     value={code}
                     onChange={(e) => setCode(e.target.value)}
                     spellCheck={false}
                 />
+                */}
 
                 <SyntaxHighlighter language="python">
                     {code}
